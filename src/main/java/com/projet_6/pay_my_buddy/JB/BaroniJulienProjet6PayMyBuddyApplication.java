b8227c059ea711c62cc89927eaa9ee4f2246cc7d
Julien Baroni <jul.baroni@orange.fr>
2022-05-10 12:31:28 +0200

    Initial commit : liaison avec remote

---
+++ src/main/java/com/projet_6/pay_my_buddy/JB/BaroniJulienProjet6PayMyBuddyApplication.java
@@ -0,0 +1,14 @@
+package com.projet_6.pay_my_buddy.JB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaroniJulienProjet6PayMyBuddyApplication {
+
+    //TODO : externaliser les configurations de connection à la base de donnée
+    public static void main(String[] args) {
+        SpringApplication.run(BaroniJulienProjet6PayMyBuddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projet_6/pay_my_buddy/JB/BaroniJulienProjet6PayMyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet_6.pay_my_buddy.JB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaroniJulienProjet6PayMyBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
